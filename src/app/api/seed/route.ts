06c7c13efa82d8832ecef463ab4f4f478f82712f
Patricio Bonetto <patricio.bonetto@outlook.com>
2024-08-24 20:49:56 -0300

    Seed actualizado para insertar clientes y unidades junto con reservas

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -3,7 +3,17 @@ import { NextResponse, NextRequest } from 'next/server';
 import { initialData } from '../../../seed/data/seed.data';
 
 export async function GET(request: Request) {
+	await prisma.cliente.deleteMany({});
+	await prisma.unidad.deleteMany({});
 	await prisma.reserva.deleteMany({});
+
+	const clientes = await prisma.cliente.createMany({
+		data: [...initialData.clientes],
+	});
+
+	const unidades = await prisma.unidad.createMany({
+		data: [...initialData.unidades],
+	});
 
 	const reservas = await prisma.reserva.createMany({
 		data: [...initialData.reservas],
--- src/seed/data/seed.data.ts
+++ src/seed/data/seed.data.ts
@@ -1,21 +1,4 @@
 export const initialData = {
-	alojamientos: [
-		{
-			//id: 1
-			nombre: 'Cabañas Alto las Flores',
-			direccion: 'Julio Cortazar S/N',
-			ciudad: 'Santa Rosa de Calamuchita',
-			provincia: 'Córdoba',
-		},
-		{
-			//id: 2
-			nombre: 'Edificio Venezia 8',
-			direccion: 'Lima 1169',
-			ciudad: 'Cordoba',
-			provincia: 'Córdoba',
-		},
-	],
-
 	unidades: [
 		{
 			//id: 1
@@ -34,6 +17,19 @@ export const initialData = {
 		},
 	],
 
+	clientes: [
+		{
+			nombre: 'Chandler Bing',
+			telefono: '9093829831',
+			email: 'chandler.bing@gmail.com',
+		},
+		{
+			nombre: 'Monica Geller',
+			telefono: '9093859831',
+			email: 'monica.geller@gmail.com',
+		},
+	],
+
 	reservas: [
 		{
 			fechaInicio: '2024-08-23T14:00:00Z',
@@ -41,18 +37,24 @@ export const initialData = {
 			cantidadPersonas: 4,
 			precioTotal: 40000,
 			pagoParcial: 10000,
+			unidadId: 1,
+			clienteId: 1,
 		},
 		{
 			fechaInicio: '2024-09-02T14:00:00Z',
 			fechaFin: '2024-09-05T10:00:00Z',
 			cantidadPersonas: 2,
 			precioTotal: 30000,
+			unidadId: 2,
+			clienteId: 2,
 		},
 		{
 			fechaInicio: '2024-09-05T14:00:00Z',
 			fechaFin: '2024-09-10T10:00:00Z',
 			cantidadPersonas: 4,
 			precioTotal: 50000,
+			unidadId: 1,
+			clienteId: 1,
 		},
 	],
 };
