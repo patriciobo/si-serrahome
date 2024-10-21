96f46fab470fd167aa0f69005517983905636c69
Patricio Bonetto <patricio.bonetto@outlook.com>
2024-10-21 16:30:41 -0300

    Se agrega datepicker y combos para propiedades y unidades, tambien se agregar validaciones para mostrar solo unidades con disponibilidad en las fechas, capacidad y propiedad seleccionadas

--- src/actions/unidades/unidades.ts
+++ src/actions/unidades/unidades.ts
@@ -5,59 +5,86 @@ import { Unidad } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 
 interface NuevaUnidadInput {
-  tipoUnidad: string;
-  nombre: string;
-  capacidad: number;
-  servicios: string[];
-  precioPorNoche?: number; 
-  imagenes: string[];
+	tipoUnidad: string;
+	nombre: string;
+	capacidad: number;
+	servicios: string[];
+	precioPorNoche?: number;
+	imagenes: string[];
 }
 
-export const insertarUnidad = async (unidadInput: NuevaUnidadInput) => {
-  
-  if (!unidadInput.tipoUnidad) {
-    throw new Error('El tipo de unidad es obligatorio.');
-  }
-
-  if (!unidadInput.nombre) {
-    throw new Error('El nombre de la unidad es obligatorio.');
-  }
-
-  if (!unidadInput.capacidad || unidadInput.capacidad <= 0 || !Number.isInteger(unidadInput.capacidad)) {
-    throw new Error('La capacidad debe ser un número mayor a 0 y no debe contener decimales.');
-  }
-
-  if (unidadInput.precioPorNoche !== undefined && isNaN(unidadInput.precioPorNoche)) {
-    throw new Error('El precio por noche debe ser un valor numérico.');
-  }
-
-  if (!Array.isArray(unidadInput.servicios) || unidadInput.servicios.length === 0) {
-    throw new Error('Debe seleccionar al menos un servicio.');
-  }
-
-  if (!Array.isArray(unidadInput.imagenes) || unidadInput.imagenes.length === 0) {
-    throw new Error('Debe proporcionar al menos una imagen');
-  }
-
-  try {
-    const unidadCreada = await prisma.unidad.create({
-      data: {
-        tipoUnidad: unidadInput.tipoUnidad,
-        nombre: unidadInput.nombre,
-        capacidad: unidadInput.capacidad,
-        servicios: unidadInput.servicios,
-        precioPorNoche: unidadInput.precioPorNoche,
-        imagenes: unidadInput.imagenes,
-      },
-    });
-
-    
-    revalidatePath('/dashboard/unidades');
-    console.log('Unidad Creada: ', unidadCreada);
-
-    return unidadCreada;
-  } catch (error) {
-    throw new Error(`Error: ${error}`);
-  }
+export const getUnidadesPorPropiedad = async (propiedadId): Unidad[] => {
+	try {
+		console.log('propId: ', propiedadId);
+		const unidades = await prisma.unidad.findMany({
+			where: {
+				propiedadId: +propiedadId,
+			},
+		});
+		return unidades;
+	} catch (error) {
+		console.log('Error: ', error);
+		throw new Error('Error al obtener las unidades', error);
+	}
 };
 
+export const insertarUnidad = async (unidadInput: NuevaUnidadInput) => {
+	if (!unidadInput.tipoUnidad) {
+		throw new Error('El tipo de unidad es obligatorio.');
+	}
+
+	if (!unidadInput.nombre) {
+		throw new Error('El nombre de la unidad es obligatorio.');
+	}
+
+	if (
+		!unidadInput.capacidad ||
+		unidadInput.capacidad <= 0 ||
+		!Number.isInteger(unidadInput.capacidad)
+	) {
+		throw new Error(
+			'La capacidad debe ser un número mayor a 0 y no debe contener decimales.'
+		);
+	}
+
+	if (
+		unidadInput.precioPorNoche !== undefined &&
+		isNaN(unidadInput.precioPorNoche)
+	) {
+		throw new Error('El precio por noche debe ser un valor numérico.');
+	}
+
+	if (
+		!Array.isArray(unidadInput.servicios) ||
+		unidadInput.servicios.length === 0
+	) {
+		throw new Error('Debe seleccionar al menos un servicio.');
+	}
+
+	if (
+		!Array.isArray(unidadInput.imagenes) ||
+		unidadInput.imagenes.length === 0
+	) {
+		throw new Error('Debe proporcionar al menos una imagen');
+	}
+
+	try {
+		const unidadCreada = await prisma.unidad.create({
+			data: {
+				tipoUnidad: unidadInput.tipoUnidad,
+				nombre: unidadInput.nombre,
+				capacidad: unidadInput.capacidad,
+				servicios: unidadInput.servicios,
+				precioPorNoche: unidadInput.precioPorNoche,
+				imagenes: unidadInput.imagenes,
+			},
+		});
+
+		revalidatePath('/dashboard/unidades');
+		console.log('Unidad Creada: ', unidadCreada);
+
+		return unidadCreada;
+	} catch (error) {
+		throw new Error(`Error: ${error}`);
+	}
+};
--- src/components/Reservas/Tabla.tsx
+++ src/components/Reservas/Tabla.tsx
@@ -1,184 +1,58 @@
-import React from "react";
-import { EstadoReserva, Reserva } from "@prisma/client";
-import { BiEnvelope } from "react-icons/bi";
-import { BsWhatsapp } from "react-icons/bs";
-import { Paginacion } from "../commons/Paginacion";
+import React from 'react';
+import { EstadoReserva, Reserva } from '@prisma/client';
+import { BiEnvelope } from 'react-icons/bi';
+import { BsWhatsapp } from 'react-icons/bs';
+import { Paginacion } from '../commons/Paginacion';
 
 interface Props {
-  reservas: Reserva[];
-  cantidadPaginas: number;
-  totalReservas: number;
-  paginaActual: number;
+	reservas: Reserva[];
+	cantidadPaginas: number;
+	totalReservas: number;
+	paginaActual: number;
 }
 
 export const Tabla = ({
-  reservas = [],
-  totalReservas,
-  cantidadPaginas,
+	reservas = [],
+	totalReservas,
+	cantidadPaginas,
 }: Props) => {
-  return (
-    <div className="mx-auto">
-      <div className="w-full flex justify-between items-center mb-3 mt-1 pl-3">
-        <div>
-          <h3 className="text-md font-semibold text-slate-800">Reservas</h3>
-        </div>
-        <div className="ml-3">
-          <div className="w-full max-w-sm min-w-[200px] relative">
-            <div className="relative">
-              <input
-                className="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-                placeholder="Buscar Reserva..."
-              />
-              <button
-                className="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
-                type="button"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="3"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-slate-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+	return (
+		<div className='mx-auto'>
+			<div className='w-full flex justify-between items-center mb-3 mt-1 pl-3'>
+				<div>
+					<h3 className='text-md font-semibold text-slate-800'>Reservas</h3>
+				</div>
+				<div className='ml-3'>
+					<div className='w-full max-w-sm min-w-[200px] relative'>
+						<div className='relative'>
+							<input
+								className='bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md'
+								placeholder='Buscar Reserva...'
+							/>
+							<button
+								className='absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded '
+								type='button'
+							>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth='3'
+									stroke='currentColor'
+									className='w-8 h-8 text-slate-600'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+									/>
+								</svg>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 
-<<<<<<< Updated upstream
-      <div className="relative flex flex-col  w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
-        <table className="w-full text-center text-sm table-auto min-w-max">
-          <thead>
-            <tr>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Cliente
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Contacto
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Fecha de Creación
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Fecha Inicio
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Fecha Fin
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Estado
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Pago Parcial
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className=" font-normal leading-none text-slate-500">
-                  Precio Total
-                </p>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {reservas.map((reserva) => (
-              <tr
-                key={reserva.id}
-                className="hover:bg-slate-50 border-b border-slate-200"
-              >
-                <td className="p-4 py-5">
-                  <p className="block font-semibold text-slate-800">
-                    {reserva.cliente.nombre}
-                  </p>
-                </td>
-                <td className="p-4 py-5">
-                  <div className="flex items-center font-semibold text-verdeIntermedio">
-                    <BsWhatsapp className="text-md" />
-                    <a
-                      href={`https://wa.me/${reserva.cliente.telefono}/`}
-                      className="ml-2"
-                    >
-                      {reserva.cliente.telefono}
-                    </a>
-                  </div>
-                  {reserva.cliente.email && (
-                    <div className="flex items-center font-semibold text-blue-400">
-                      <BiEnvelope className="text-xl" />
-                      <p className="block ml-2 font-semibold text-blue-400">
-                        {reserva.cliente.email}
-                      </p>
-                    </div>
-                  )}
-                </td>
-                <td className="p-4 py-5">
-                  <p className="font-bold text-slate-500">
-                    {reserva.fechaHoraCreacion.toLocaleDateString("es-AR", {
-                      timeZone: "GMT",
-                    })}
-                  </p>
-                </td>
-                <td className="p-4 py-5">
-                  <p className="font-bold text-slate-500">
-                    {reserva.fechaInicio.toLocaleDateString("es-AR", {
-                      timeZone: "GMT",
-                    })}
-                  </p>
-                </td>
-                <td className="p-4 py-5">
-                  <p className="font-bold text-slate-500">
-                    {reserva.fechaFin.toLocaleDateString("es-AR", {
-                      timeZone: "GMT",
-                    })}
-                  </p>
-                </td>
-                <td className="p-4 py-5">
-                  <div
-                    className={` text-center select-none whitespace-nowrap rounded-lg py-2 px-3.5 align-baseline font-sans font-semibold uppercase leading-none text-white ${
-                      reserva.estado === EstadoReserva.PENDIENTE
-                        ? "bg-amber-400"
-                        : reserva.estado === EstadoReserva.PAGO_PARCIAL
-                        ? "bg-verdeClaro"
-                        : reserva.estado === EstadoReserva.PAGADA
-                        ? "bg-verdeIntermedio"
-                        : reserva.estado === EstadoReserva.CANCELADA
-                        ? "bg-red-500"
-                        : ""
-                    }`}
-                  >
-                    {reserva.estado}
-                  </div>
-                </td>
-                <td className="p-4 py-5">
-                  <p className=" text-slate-500">${reserva.pagoParcial}</p>
-                </td>
-                <td className="p-4 py-5">
-                  <p className=" text-slate-500">${reserva.precioTotal}</p>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-=======
 			<div className='relative flex flex-col  w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border'>
 				<table className='w-full text-center text-sm table-auto min-w-max'>
 					<thead>
@@ -311,16 +185,15 @@ export const Tabla = ({
 						))}
 					</tbody>
 				</table>
->>>>>>> Stashed changes
 
-        <Paginacion
-          totalRegistros={totalReservas}
-          cantidadPaginas={cantidadPaginas}
-          entidad="Reservas"
-        />
-      </div>
-    </div>
-  );
+				<Paginacion
+					totalRegistros={totalReservas}
+					cantidadPaginas={cantidadPaginas}
+					entidad='Reservas'
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Tabla;
